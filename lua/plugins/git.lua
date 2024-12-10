939c01e9ecc0270a2e3943f3d317ac01f463ca9d
johnpgr <jp.greidinger@hotmail.com>
2024-12-10 10:20:04 -0300

    Neogit config update

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,11 +7,11 @@ return {
     },
     cmd = "Neogit",
     opts = {
-        -- commit_editor = {
-        --     kind = "vsplit",
-        --     show_staged_diff = false,
-        -- },
-        -- console_timeout = 5000,
-        -- auto_show_console = false,
+        commit_editor = {
+            kind = "vsplit",
+            show_staged_diff = false,
+        },
+        console_timeout = 5000,
+        auto_show_console = false,
     },
 }
